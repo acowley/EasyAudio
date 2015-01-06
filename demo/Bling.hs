a22ce2870a802cd616747bdf137f844fedb8a2c8
Greg Hale <imalsogreg@gmail.com>
2015-01-05 21:07:59 -0500

    Add demo wav as data-file

--- demo/Bling.hs
+++ demo/Bling.hs
@@ -1,13 +1,16 @@
 import Control.Monad (when)
 import EasyAudio
 import Graphics.UI.SDL as SDL
+import Paths_EasyAudio
+import System.FilePath
 
 main :: IO ()
 main = do r <- SDL.init SDL.SDL_INIT_AUDIO
           when (r < 0) (error "Error initializing SDL")
           putStrLn "Initialized SDL"
           (loadClip, cleanup) <- easyAudio
-          vader <- loadClip "swvader04.wav"
+          audioFilePath <- getDataFileName ("demo" </> "bling.wav")
+          vader <- loadClip audioFilePath
           vader
           cleanup
           putStrLn "Quitting SDL"
